7be25a9e1840065b735cfc34213bd38fb5710202
Anastasiia Snihur <snihur.anastasiia@gmail.com>
2025-04-13 15:14:43 +0300

    app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,53 @@
 import React, { useEffect } from "react";
-import './App.scss';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/homepage/Homepage';
-import Registration from './pages/registration/Registration';
-import Header from './components/header/Header';
-import { useLocation } from 'react-router-dom';
-import LoginPage from './pages/loginpage/Loginpage';
-import PetRegisterPage from './pages/petregister/PetRegisterPage';
-import AnimalPage from './pages/animalpage/AnimalPage';
-import PetStoriesPage from './pages/petstories/PetStoriesPage';
-import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
+import { Routes, Route, useLocation } from "react-router-dom";
 import AOS from "aos";
+
+import "./App.scss";
 import "aos/dist/aos.css";
+
+// Components
+import Header from "./components/header/Header";
+import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
 import Logout from "./components/auth/Logout";
+
+// Pages
+import HomePage from "./pages/homepage/Homepage";
+import Registration from "./pages/registration/Registration";
+import LoginPage from "./pages/loginpage/Loginpage";
+import PetRegisterPage from "./pages/petregister/PetRegisterPage";
+import AnimalPage from "./pages/animalpage/AnimalPage";
+import PetStoriesPage from "./pages/petstories/PetStoriesPage";
+import Contact from "./components/ask-about/Contact";
+
+// Dashboards
 import VolunteerDashboard from "./components/dashboard/VolunteerDashboard";
 import ShelterDashboard from "./components/dashboard/ShelterDashboard";
-import Contact from "./components/ask-about/Contact";
 
 function App() {
   const location = useLocation();
 
-  const hideHeaderRoutes = ['/cabinet']; // або ['/login', '/register'] залежно від потреб
+  const hideHeaderRoutes = ["/cabinet"];
 
   useEffect(() => {
     AOS.init({ duration: 800, once: true });
   }, []);
 
   return (
-    <div className='app-container'>
+    <div className="app-container">
       <ScrollToTop />
       {!hideHeaderRoutes.includes(location.pathname) && <Header />}
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/registration" element={<Registration />} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/petregister" element={<PetRegisterPage/>} />
-        <Route path="/animalpage" element={<AnimalPage/>} />
-        <Route path="/petstories" element={<PetStoriesPage/>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/petregister" element={<PetRegisterPage />} />
+        <Route path="/animalpage" element={<AnimalPage />} />
+        <Route path="/petstories" element={<PetStoriesPage />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/volunteer-dashboard" element={<VolunteerDashboard />} />
         <Route path="/shelter-dashboard" element={<ShelterDashboard />} />
         <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/cabinet" element={<CabinetHeader />} /> ← або інша логіка */}
       </Routes>
     </div>
   );
